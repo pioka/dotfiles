724502f5b6c7db7af7956768714ac59e72ebd2d9
pioka <20146647+pioka@users.noreply.github.com>
2023-01-22 02:50:01 +0900

    zsh/init-tools: ツールバージョンを変数にまとめる

--- files/.zsh/init-tools.zsh
+++ files/.zsh/init-tools.zsh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 
+V_ASDF='v0.8.1'
+V_DELTA='0.12.1'
+V_NVIM='stable'
+
 mkdir -p ~/.local/bin
 
 # zsh-auto-suggestions
@@ -10,29 +14,21 @@ source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 # asdf
 if [ ! -f ~/.asdf/asdf.sh ]; then
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf -b v0.8.1
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf -b ${V_ASDF}
 fi
 source ~/.asdf/asdf.sh
 
-# nodejs (for coc.nvim)
-if ! node -v > /dev/null; then
-  asdf plugin-add nodejs
-  asdf install nodejs lts
-  asdf global nodejs lts
-fi
-
 # neovim
 if [ ! -x ~/.local/bin/nvim ];then
-  curl -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -o ~/.local/bin/nvim
+  curl -L https://github.com/neovim/neovim/releases/download/${V_NVIM}/nvim.appimage -o ~/.local/bin/nvim
   chmod +x ~/.local/bin/nvim
 fi
 
 # git-delta
 if [ ! -x ~/.local/bin/delta ]; then
-  curl -L https://github.com/dandavison/delta/releases/download/0.12.1/delta-0.12.1-x86_64-unknown-linux-gnu.tar.gz -o ~/.local/bin/delta.tar.gz
+  curl -L https://github.com/dandavison/delta/releases/download/${V_DELTA}/delta-${V_DELTA}-x86_64-unknown-linux-gnu.tar.gz -o ~/.local/bin/delta.tar.gz
   tar -xzf ~/.local/bin/delta.tar.gz --wildcards --strip-components=1 -C ~/.local/bin '**/delta' && rm ~/.local/bin/delta.tar.gz
 fi
-
 
 # git 
 if [ ! -f ~/.gitconfig.local ]; then
@@ -42,4 +38,3 @@ if [ ! -f ~/.gitconfig.local ]; then
 	name = ''
 EOS
 fi
-
